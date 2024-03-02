b469090baf4eedb47a92d2a3d747c8d8cb3712ed
SquareWoter-4000 <Furmich231@gmail.com>
2024-03-01 22:38:51 -0500

    change from neoforge to forge

--- src/main/java/mod/azure/darkwaters/DarkWatersMod.java
+++ src/main/java/mod/azure/darkwaters/DarkWatersMod.java
@@ -11,10 +11,6 @@ import mod.azure.darkwaters.entity.helper.AttackType;
 import mod.azure.darkwaters.util.DarkWatersMobs;
 import mod.azure.darkwaters.util.DarkWatersSounds;
 import mod.azure.darkwaters.util.DarkWatersSpawning;
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
@@ -29,10 +25,13 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(DarkWatersMod.MODID)
 public class DarkWatersMod {
@@ -60,12 +59,28 @@ public class DarkWatersMod {
 	};
 
 	private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-	public static AzureSpawnEgg ABERRATION_SPAWN_EGG;
-	public static AzureSpawnEgg MANARAW_SPAWN_EGG;
-	public static AzureSpawnEgg MOHAST_SPAWN_EGG;
-	public static AzureSpawnEgg SIGHT_HUNTER_SPAWN_EGG;
-	public static AzureSpawnEgg CRAEKEN_SPAWN_EGG;
-	public static AzureSpawnEgg MIRAID_SPAWN_EGG;
+	public static final RegistryObject<AzureSpawnEgg> ABERRATION_SPAWN_EGG = ITEMS.register("aberration_spawn_egg", () -> new AzureSpawnEgg(DarkWatersMobs.ABERRATION,0x150056,0x826ccc));
+	public static final RegistryObject<AzureSpawnEgg> MANARAW_SPAWN_EGG = ITEMS.register("manaraw_spawn_egg", () -> new AzureSpawnEgg(DarkWatersMobs.MANARAW, 0x181c59, 0x636b6d));
+	public static final RegistryObject<AzureSpawnEgg> MOHAST_SPAWN_EGG = ITEMS.register("mohast_spawn_egg", () -> new AzureSpawnEgg(DarkWatersMobs.MOHAST, 0x477385, 0xacb7b7));
+	public static final RegistryObject<AzureSpawnEgg> SIGHT_HUNTER_SPAWN_EGG = ITEMS.register("sighthunter_spawn_egg", () -> new AzureSpawnEgg(DarkWatersMobs.SIGHT_HUNTER,0x01293a, 0x808f95));
+	public static final RegistryObject<AzureSpawnEgg> CRAEKEN_SPAWN_EGG = ITEMS.register("craeken_spawn_egg", () -> new AzureSpawnEgg(DarkWatersMobs.CRAEKEN, 0xada7a2, 0xcee3e3));
+	public static final RegistryObject<AzureSpawnEgg> MIRAID_SPAWN_EGG = ITEMS.register("miraid_spawn_egg", () -> new AzureSpawnEgg(DarkWatersMobs.MIRAID, 0x5d5d6e, 0xd6d6d6));
+
+	public static final DeferredRegister<CreativeModeTab> ITEM_GROUPS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+	public static final RegistryObject<CreativeModeTab> DW_CREATIVE_TAB = ITEM_GROUPS.register("dw_tab", () -> CreativeModeTab.builder()
+			.title(Component.translatable("itemGroup.darkwaters.itemgroup"))
+			.icon(() -> new ItemStack(ABERRATION_SPAWN_EGG.get()))
+			.displayItems((params, output) -> {
+				output.accept(ABERRATION_SPAWN_EGG.get());
+				output.accept(MANARAW_SPAWN_EGG.get());
+				output.accept(MOHAST_SPAWN_EGG.get());
+				output.accept(SIGHT_HUNTER_SPAWN_EGG.get());
+				output.accept(CRAEKEN_SPAWN_EGG.get());
+				output.accept(MIRAID_SPAWN_EGG.get());
+			}).build());
+
+	public static final DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, MODID);
 
 	public static final ResourceLocation modResource(String name) {
 		return new ResourceLocation(MODID, name);
@@ -76,6 +91,17 @@ public class DarkWatersMod {
 		DarkWatersMobs.init();
 		DarkWatersSounds.init();
 		EntityDataSerializers.registerSerializer(ATTACK_TYPE);
+
+
+		MOB_EFFECTS.register("storm_darkness", () -> STORMDARKNESS);
+
+		IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+		IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+
+		MOB_EFFECTS.register(modBus);
+		ITEMS.register(modBus);
+		ITEM_GROUPS.register(modBus);
+
 
 		/*
 		ABERRATION_SPAWN_EGG = Registry.register(BuiltInRegistries.ITEM, modResource("aberration_spawn_egg"), new AzureSpawnEgg(DarkWatersMobs.ABERRATION, 0x150056, 0x826ccc));
@@ -98,8 +124,6 @@ public class DarkWatersMod {
 				}).build()); // build() no longer registers by itself
 
 		 fabric reference */
-
-		ABERRATION_SPAWN_EGG =Registry.register(BuiltInRegistries.ITEM, modResource("aberration_spawn_egg"), new AzureSpawnEgg(DarkWatersMobs.ABERRATION, 0x150056, 0x826ccc));
 
 		DarkWatersSpawning.addSpawnEntries();
 	}
